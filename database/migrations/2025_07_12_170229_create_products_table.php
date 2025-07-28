6cb2cc5d13f39f4886056198813370b131d34e7c
ysmndiyah <sakdiyahh845@gmail.com>
2025-07-28 17:41:16 +0700

    yasminpush

--- database/migrations/2025_07_12_170229_create_products_table.php
+++ database/migrations/2025_07_12_170229_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
         $table->integer('stok');
         $table->decimal('harga', 10, 2);
         $table->timestamps();
-        $table->varchar('gambar', 255);
+        $table->string('gambar', 255);
     });
 }
 
